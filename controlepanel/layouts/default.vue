9b8d93092579146d084ccf70d96781c7998d351c
caninus <caninus@caninuss-MacBook-Pro.local>
2024-02-04 21:55:25 +0900

    add pages,layouts

---
+++ controlepanel/layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>default.vue</div>
+    <slot />
+</template>
+<script setup>
+
+</script>
+<style scoped>
+
+</style>
---
+++ controlepanel/pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+<div>index.vue</div>
+</template>
+<script setup>
+
+</script>
+<style scoped>
+
+</style>
