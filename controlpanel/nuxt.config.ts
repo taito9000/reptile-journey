2d902a08b5c08be7d346ec18d29394cabd3bdf29
caninus <caninus@caninuss-MacBook-Pro.local>
2024-02-05 23:50:51 +0900

    add runtimeconfig

--- controlpanel/nuxt.config.ts
+++ controlpanel/nuxt.config.ts
@@ -8,5 +8,16 @@ export default defineNuxtConfig({
       // publicDir: process.env.ENV !== 'prod' ? '../public-dev' : '../public-prod'
       publicDir:'../public-dev'
     }
+  },
+  runtimeConfig: {
+    public: {
+      ApiKey: process.env.FIREBASE_APIKEY,
+      AuthDomain: process.env.FIREBASE_AUTHDOMAIN,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      StorageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      MessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      AppId: process.env.FIREBASE_APP_ID,
+      MeasurementId: process.env.FIREBASE_MEASUREMENTID,
+    },
   }
 })
