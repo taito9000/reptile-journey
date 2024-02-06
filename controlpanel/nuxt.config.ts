eea45cf2abfff6fd0cbda169e4f882601389b42e
caninus <caninus@caninuss-MacBook-Pro.local>
2024-02-06 21:57:18 +0900

    vuetify installed !!

--- controlpanel/nuxt.config.ts
+++ controlpanel/nuxt.config.ts
@@ -1,4 +1,6 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+
+import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
+
 export default defineNuxtConfig({
   ssr: false,
   devtools: { enabled: true },
@@ -18,6 +20,24 @@ export default defineNuxtConfig({
       MessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
       AppId: process.env.FIREBASE_APP_ID,
       MeasurementId: process.env.FIREBASE_MEASUREMENTID,
+    },
+  },
+  build: {
+    transpile: ["vuetify"],
+  },
+  modules: [
+    (_options, nuxt) => {
+      nuxt.hooks.hook("vite:extendConfig", (config) => {
+        // @ts-expect-error
+        config.plugins.push(vuetify({ autoImport: true }));
+      });
+    },
+  ],
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
     },
   },
 });
